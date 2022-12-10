7a4d073fa28175903f977483f05db6173bffce21
Yuriy Savchenko <47382042+yurkaronin@users.noreply.github.com>
2022-12-10 19:56:13 +0300

    Добавил в проект плавный скрол при прокрутке страницы

--- components/scripts_include.php
+++ components/scripts_include.php
@@ -3,4 +3,5 @@
 <script src="./libs/itc-custom-select.js"></script>
 <script src="./libs/fancybox.umd.js"></script>
 <script src="https://api-maps.yandex.ru/2.1/?apikey=1dba075a-a65a-4b22-84a2-ebb26f26fe43&lang=ru_RU" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/smoothscroll/1.4.10/SmoothScroll.min.js" integrity="sha256-huW7yWl7tNfP7lGk46XE+Sp0nCotjzYodhVKlwaNeco=" crossorigin="anonymous"></script>
 <script src="./js/scripts.js"></script>
