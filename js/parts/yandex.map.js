0ca17ea840a71c75c4839a366f15a33dc1497470
RoninsWork <yurkaronin@gmail.com>
2022-12-12 13:06:11 +0300

    Отключил скролл карты пальцем

--- js/parts/yandex.map.js
+++ js/parts/yandex.map.js
@@ -1,5 +1,5 @@
 if (document.querySelector('.map')) {
-  let center = [45.13542039095159, 39.02629313757318];
+  let center = [54.314192, 48.403132];
 
 
   function init() {
@@ -9,7 +9,7 @@ if (document.querySelector('.map')) {
       zoom: 17
     });
 
-    let placemarRkschool = new ymaps.Placemark([45.13542039095159, 39.02629313757318], {}, {
+    let placemarRkschool = new ymaps.Placemark([54.314192, 48.403132], {}, {
       iconLayout: 'default#image', /* говорим что будем отображать на карте в качестве геометки  */
       iconImageHref: './img/map/balun.svg', /* указываем пусть к картинке на нашем сайте  */
       iconImageSize: [48, 64], /* размеры картинки  */
@@ -20,10 +20,11 @@ if (document.querySelector('.map')) {
     map.controls.remove('searchControl'); // удаляем поиск
     map.controls.remove('trafficControl'); // удаляем контроль трафика
     map.controls.remove('typeSelector'); // удаляем тип
-    // map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-    // map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+    map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+    map.controls.remove('zoomControl'); // удаляем контрол зуммирования
     map.controls.remove('rulerControl'); // удаляем контрол правил
     map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+    map.behaviors.disable('drag'); // — это отключает прокрутку карты, но есть одно "но": сама страница также не прокручивается.
 
     map.geoObjects.add(placemarRkschool);
 
