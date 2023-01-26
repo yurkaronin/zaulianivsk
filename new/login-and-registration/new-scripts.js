d4432f4bb5c36c200afef2bd49cdca71874b0c1b
Yuriy Savchenko <47382042+yurkaronin@users.noreply.github.com>
2023-01-26 10:28:54 +0300

    Добавил подпапку для новыэ компонентов сайта

---
+++ new/login-and-registration/new-scripts.js
@@ -0,0 +1,6 @@
+if (document.querySelector('#select-6')) {
+  const select4 = new ItcCustomSelect('#select-6');
+};
+if (document.querySelector('#select-7')) {
+  const select5 = new ItcCustomSelect('#select-7');
+};
